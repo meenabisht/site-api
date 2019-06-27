a06b802295f4301db41269c7f61b6c1f68f4b553
meenabisht <meena.bisht392@gmail.com>
2019-06-27 18:22:09 +0530

    weather module

---
+++ web/modules/custom/weather/src/WeatherService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Drupal\weather;
+
+use Guzzle\Http\Client;
+
+class WeatherService {  
+  public function WeatherMethod($city) {
+    $app = \Drupal::config('weather.settings')->get('app');
+    $client = new \GuzzleHttp\Client();
+    $response = $client->get('https://samples.openweathermap.org/data/2.5/weather?q='.$city.'&appid='.$app);
+    // kint($response);
+    // die();   
+    return $response->getBody()->getContents();
+  }
+}
